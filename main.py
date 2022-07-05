e4f62bf37d3a39257a471cf9f1aae8f52fdbd37b
Vanshaj Bawa <vbawa@amazon.com>
2022-07-05 10:10:37 -0400

    Added direction parameter for 3 run-throughs
    3 total checks, forwards direction, forwardsAgain direction, and backwards direction to check all docs exist in source to target, the random sample of docs from source are the exact same in target, and all docs in target exist in source. Got rid of reverse_check helper method as it would be easier to just call the same function. Also if'ed out certain portions from the method to avoid redundancies.

--- main.py
+++ main.py
@@ -5,64 +5,110 @@ from bson.json_util import dumps
 from deepdiff import DeepDiff 
 from tqdm import tqdm
 
-def dataCompare(source_uri, target_uri, db1, db2, coll1, coll2, percent):
+def data_compare(source_uri, target_uri, db1, db2, coll1, coll2, percent, direction):
     client1 = pymongo.MongoClient(source_uri, serverSelectionTimeoutMS=5000)
     client2 = pymongo.MongoClient(target_uri, serverSelectionTimeoutMS=5000)
     first_database = client1[db1]
     second_database = client2[db2]
     first_collection = first_database[coll1]
     second_collection = second_database[coll2]
-    #get a random sample of documents from the source based on the percent the user inputs and store in sampled_docs
-    sampled_docs = get_rand_sample_docs(first_collection, second_collection,percent)
-    #Loop through all sampled_docs find_one based on matching ID in second collection and store the result of that find in queried_doc
+
+    #method will check edge cases to see if any of the collections passed in are empty, or if the 2 collections dont match each other in document length
+    empty_coll_or_diff_num_docs_check(first_collection, second_collection) 
+
+    """Based on what the direction is when the function is called (default direction value from the main method is "forwards"), it will get a different set 
+    of documents called sampled_docs to hold which will be used to loop through in the later code and.  
+    
+    There are 3 total cases here for direction: 
+        1) "forwards" only checks to see if ALL (so 100% is passed here) documents from source collection are present in the target collection 
+        2) "forwardsAgain" does document level diff'ing to see if there are any actual differences based on the user-selected percentage of sample docs 
+        3) "backwards" checks to see if ALL (once again 100% is passed here) documents from target collection are present in the source collection
+    
+    Also based on the direction, the find_one_coll is specified which is specifying which collection we are going to do the find_one on as we loop 
+    through the sampled_docs. So for example, in the forwards case we will do a find_one on collection2 which would be the target namespace we are 
+    looking for those documents in."""
+
+    if direction == "forwards":
+        sampled_docs = get_rand_sample_docs(first_collection, 100)
+        find_one_coll = second_collection
+        print("\nProgress bar is checking to see if all documents in source collection exist in the target collection...")
+    if direction == "forwardsAgain":
+        sampled_docs = get_rand_sample_docs(first_collection, percent)
+        find_one_coll = second_collection
+        print("\nProgress bar this time is checking to see if based on the percent you selected, those random sample of documents match exactly from source -> target...")
+    if direction == "backwards":
+        sampled_docs = get_rand_sample_docs(second_collection, 100)
+        find_one_coll = first_collection
+        print("\nReverse Checking! Progress bar is lastly checking to see if all documents in target collection exist in the source collection...")
+
+    #tqdm is the progress bar the terminal outputs to help the user see the progress
     for document in tqdm(sampled_docs): 
-        queried_doc = second_collection.find_one({"_id": document["_id"]})
-        #If it cannot find any matching ID document in queried_doc then tell user the doc was not found in target from source 
-        if queried_doc == None:
-            str = "The following document based on it's ID was not found from teh source in the target:\n{0}"
-            print(str.format(document))
-            return False
-        pretty_q_doc_str = dumps(queried_doc, indent = 4, separators =("", " = "))
-        pretty_document_str = dumps(document, indent = 4, separators =("", " = "))
-        #compare the strings of the documents to ensure they are equal for each document 
-        if pretty_document_str == pretty_q_doc_str: 
-            continue
-        #if the dumps strings are not equal, it can be 2 cases: first case is that the right keys with the right values are there, just in the wrong order, 
-        #or the second case is there is guranteed differences such as some portion missing/extra portion 
-        else: 
-            # DeepDiff is a package that compares 2 dictionaries, 2 iterables, 2 strings or 2 other objects and returns their differences as a deepdiff.diff
-            # It is important to note however, that DeepDiff does not check for order of key/value pairs, so although our dumps strings earlier could have had 
-            # a difference (due to order being different) DeepDiff would not return anything as a difference. So for combatting this issue, a check is required 
-            # to see if the length of the result of that DeepDiff call = 0, then you know that the only difference was the orders from source to target. If that 
-            # is not the case, and the length of the diff is greater than 0, then there are actual differences that you have to report those differences to the 
-            # user.
-
-            # To summarize, there are 3 paths when comparing documents: 
-            #     1) the dump strings match each other in the document comparison and then we continue. If all the documents go down this path, at the end you print that it passed and return True.
-            #     2) there is a difference with the dump strings, but the length of DeepDiff is 0, so then we know the only difference can be the order, so we print that to user and return False.
-            #     3) there is a difference with the dump strings, but the length is anything else but 0, so we know there are actual differences, so we print to user and return False.
-
-            diff = DeepDiff(document, queried_doc, verbose_level=2, report_repetition=True).pretty()
-            if len(diff) == 0:
-                str = "****Failed!**** The values are all there but the order of values is different from source to target. Source document looks like this:\n{0}\nTarget document looks like this:\n{1}"
-                print(str.format(pretty_document_str, pretty_q_doc_str))
+        queried_doc = find_one_coll.find_one({"_id": document["_id"]})
+        # Only if the direction is "forwards" or "backwards" do we check to see if the documents exist in the opposite collection as described above. "forwardsAgain" 
+        # would not need this as you already checked the whole source collection in "forwards" so a random sample checking again would be redundant.
+        if direction == "forwards" or direction == "backwards":
+            if queried_doc == None: 
+                if direction == "forwards":
+                    str = "The following document based on it's ID was not found in the target but was found in the source:\n{0}"
+                if direction == "backwards":
+                    str = "The following document based on it's ID was not found in the source but was found in the target:\n{0}"
+                print(str.format(document))
                 return False
-            else:
-                diff_str = "****Failed!**** There are differences that were found in the target doc as seen here {0}\nfrom the source doc as seen here {1}\nTHE DIFFERENCES CONSIST SPECIFICALLY OF THE FOLLOWING:\n{2}"
-                print(diff_str.format(pretty_document_str, pretty_q_doc_str, diff))
-                return False
-    #Checks to see if reverse 
-    if reverse_check(first_collection, second_collection, percent) == True:
-        print("****PASSED!**** All Chosen Data Samples Match Exactly from Source to Target!")
-        
+        #Only if direction = "forwardsAgain" then we will do the actual document comparison and difference finding 
+            pretty_q_doc_str = dumps(queried_doc, indent = 4, separators =("", " = "))
+            pretty_document_str = dumps(document, indent = 4, separators =("", " = "))
+            #compare the strings of the documents to ensure they are equal for each document 
+            if pretty_document_str == pretty_q_doc_str: 
+                continue
+            # if the dumps strings are not equal, it can be 2 cases: 
+            #     First case is that the right keys with the right values are there, just in the wrong order, 
+            #     Second case is there is  guranteed differences between the documents such as for example some portion of doc missing/extra portion added.
+            else: 
+                # DeepDiff is a package that compares 2 dictionaries, 2 iterables, 2 strings or 2 other objects and returns their differences as a <class 'deepdiff.diff.DeepDiff'>.
+                # This will return to user what the differences were between the 2 objects in the output. Learn more about DeepDiff here: https://zepworks.com/deepdiff/current/basics.html
+                # It is important to note however, that DeepDiff does not check for order of key/value pairs, so although our dumps strings earlier could have had 
+                # a difference (due to order being different) DeepDiff would not return anything as a difference. So for combatting this issue, a check is required 
+                # to see if the length of the result of that DeepDiff call = 0, then you know that the only difference was the orders from source to target. If that 
+                # is not the case, and the length of the diff is greater than 0, then there are actual differences that you have to report those differences to the 
+                # user.
 
-# get_rand_sample function description 
-# 1. First this function does all the basic checks to see if either collection is empty or if one collection has more documents than the other.
-# 2. Next, it gets a rounded sample_size from the percent value the user chose to compare. 
-# 3. Lastly, it takes that sample size and via aggregation it gets a random sample of docs of that sample_size from the source and stores it into 
-#    a list and returns that list 
+                # To summarize, there are 3 paths when comparing documents: 
+                #     1) the dump strings match each other in the document comparison and then we continue. If all the documents go down this path, at the end you print that it passed and return True.
+                #     2) there is a difference with the dump strings, but the length of DeepDiff is 0, so then we know the only difference can be the order, so we print that to user and return False.
+                #     3) there is a difference with the dump strings, but the length is anything else but 0, so we know there are actual differences, so we print to user and return False.
+                diff = DeepDiff(document, queried_doc, verbose_level=2, report_repetition=True).pretty()
+                if len(diff) == 0:
+                    str = "****Failed!**** The values are all there but the order of values is different from source to target. Source document looks like this:\n{0}\nTarget document looks like this:\n{1}"
+                    print(str.format(pretty_document_str, pretty_q_doc_str))
+                    return False
+                else:
+                    diff_str = "****Failed!**** There are differences that were found in the target doc as seen here {0}\nfrom the source doc as seen here {1}\nTHE DIFFERENCES CONSIST SPECIFICALLY OF THE FOLLOWING:\n{2}"
+                    print(diff_str.format(pretty_document_str, pretty_q_doc_str, diff))
+                    return False
+    #Based on what the direction is when you are running this method, update direction variable correctly and call the function again. If it is run during "backwards" direction, 
+    #then you are at the end and then it prints a passed message to user letting them know nothing caused it fail. 
+    if direction == "forwards":
+        direction = "forwardsAgain"
+        data_compare(source_uri, target_uri, db1, db2, coll1, coll2, percent, direction)
+    elif direction == "forwardsAgain":
+        direction = "backwards"
+        data_compare(source_uri, target_uri, db1, db2, coll1, coll2, 100, direction)
+    else:
+        print("\n****PASSED!**** \n1) ✅  All documents in source collection exist in target collection\n2) ✅  All documents in target collection exist in source\n" 
+                "3) ✅  All randomly sampled documents based on your defined percentage match exactly from source to target!\n")
+        return True
 
-def get_rand_sample_docs(first_coll, second_coll, percent):
+def get_rand_sample_docs(sample_coll, percent):
+    #Convert from percent to a number of documents to sample from in the aggregation
+    sample_size = math.ceil((percent/100) * sample_coll.count_documents({}))
+
+    #Aggregate the random sample based on sample_size
+    sampled_docs = list(sample_coll.aggregate([
+        {"$sample": {"size": sample_size}}
+    ]))
+    return sampled_docs
+
+def empty_coll_or_diff_num_docs_check(first_coll, second_coll):
     coll1_num_docs = first_coll.count_documents({})
     coll2_num_docs= second_coll.count_documents({})
 
@@ -82,33 +128,6 @@ def get_rand_sample_docs(first_coll, second_coll, percent):
         print(str.format(coll1_num_docs, coll2_num_docs))
         return False
 
-    #Convert from percent to a number of documents to sample from in the aggregation
-    sample_size = math.ceil((percent/100) * coll1_num_docs)
-
-    #Aggregate the random sample based on sample_size
-    sampled_docs = list(first_coll.aggregate([
-        {"$sample": {"size": sample_size}}
-    ]))
-    return sampled_docs
-
-# reverse_check description
-# This function creates a new random sample of documents based on the same percent the user chose, this time from the target instead of source. 
-# For each document in the sample, it checks from target and compares it against the source (opposite case) to check if all documents in the sample
-# exist in target. If a document doesn't exist, it will notify the user which document is not found in source and return False. Returns True if all 
-# documents from sample in target exist in source. 
-
-def reverse_check(coll1, coll2, percent):
-    #inputting in reverse order for get_rand_sample_docs for getting a random sample of docs from coll2 this time
-    sampled_docs = get_rand_sample_docs(coll2, coll1, percent)
-    for document in sampled_docs:
-        queried_doc = coll1.find_one({"_id": document["_id"]})
-        #If it cannot find any matching ID document in queried_doc then tell user the doc was not found in target from source 
-        if queried_doc == None:
-            str = "The following document based on it's ID was not found from the target in the source:\n{0}"
-            print(str.format(document))
-            return False
-    return True
-
 #Main method
 if __name__ == "__main__":
     #order diff, same content is "s_one", "s_two"
@@ -127,4 +146,4 @@ if __name__ == "__main__":
     parser.add_argument("--percent", type=int, required=True, help="Percent of collection to compare")
 
     args = parser.parse_args()
-    dataCompare(args.source_uri, args.target_uri, args.source_namespace, args.target_namespace, args.source_collection, args.target_collection, args.percent)
+    data_compare(args.source_uri, args.target_uri, args.source_namespace, args.target_namespace, args.source_collection, args.target_collection, args.percent, "forwards")
